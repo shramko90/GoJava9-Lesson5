9e26cc3720134e0dbf2466e325f7784a38117a4f
Tiger <Shramkot@ukr.net>
2017-10-28 22:40:01 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Tiger on 28.10.2017.
+ */
+public class Main {
+}
---
+++ src/Transport/CarDoor.java
@@ -0,0 +1,7 @@
+package Transport;
+
+/**
+ * Created by Tiger on 28.10.2017.
+ */
+public class CarDoor {
+}
